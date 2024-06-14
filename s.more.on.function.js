4d1af6468ffc40043ecb1e534c30ac2daf010ccd
Siddhant Shrestha <70473153+Siddhant-0@users.noreply.github.com>
2024-06-14 17:37:57 +0545

    Update s.more.on.function.js

--- s.more.on.function.js
+++ s.more.on.function.js
@@ -1,14 +1,46 @@
 // TODO : spread vs rest
 
+
+//? Higher order function
+//? 1. callBack function (function as an arrgument to another function )
+//? 2. Closure (function returning function)
 // callback function
 
 //? function which is passed as an argument to another function
 //? is called call function
 
-const getSum = (a, b, callbackfunc) => {
-  let sum = a + b;
-  callbackfunc(sum);
-};
-getSum(5, 7, (value) => {
-  console.log(value);
-});
+// const getSum = (a, b, callbackfunc) => {
+//   let sum = a + b;
+//   callbackfunc(sum);
+// };
+// getSum(5, 7, (value) => {
+//   console.log(value);
+// });
+
+//?function returning function
+//! closure => funcrtion + lexical environment (surrounding environment )
+
+//!we can say makeAdder is a function is a function factory
+// const makeAdder = (x) => {
+//   const doSum = (y) => {
+//     let sum = x + y;
+//     return sum;
+//   };
+//   return doSum;
+// };
+
+// const doSum5 = makeAdder(5);
+// const doSum10 = makeAdder(10);
+
+// const sum = doSum10(10);
+// console.log(sum);
+
+// const sum = makeAdder(25)(5);
+
+// console.log(sum);
+
+//? aesthetic format
+
+const makeAdder = (x) => (y) => x + y;
+
+console.log(makeAdder(100)(5));
